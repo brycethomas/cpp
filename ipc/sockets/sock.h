5a341aec6fcf7461c6295bde70f72de33d24ff41
Bryce Thomas <bryce.m.thomas@gmail.com>
2020-10-15 19:09:40 -0700

    Add ipc/sockets example.

---
+++ ipc/sockets/sock.h
@@ -0,0 +1,7 @@
+// Based on
+// https://opensource.com/article/19/4/interprocess-communication-linux-networking
+#define PortNumber      9876
+#define MaxConnects        8
+#define BuffSize         256
+#define ConversationLen    3
+#define Host            "localhost"
